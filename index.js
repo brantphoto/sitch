1f77f8e2c863ac17af3118a58ca30c5fabd26760
Brant Barger <brant.barger@vroozi.com>
2017-04-25 22:28:25 -0500

    reformatted testrunner

--- index.js
+++ index.js
@@ -19,31 +19,41 @@ exports.run = function(path) {
     }
 
     for (var i = 0; i < tree.main.length; i++) {
-        if (!tree.data[tree.main[i]][0]["url"]) {
-            console.log('no url');
-            break;
-        }
-        let x = runSteps(tree.data[tree.main[i]]);
-        if (!x) break;
+        let formattedTests = formatTests(tree.data[tree.main[i]]);
+        setupTest(formattedTests[0])
+        performGiven(formattedTests[1])
+        performWhen(formattedTests[2])
+        performThen(formattedTests[3])
     }
-
 }
 
-function runSteps(steps) {
-    var result = [];
-    for (var i = 0; i < steps.length; i++) {
-        if (steps[i].given && steps[i].url) {
-            driver.get(steps[i].url);
-            console.log(colors.green('given ' + steps[i].given));
-        } else if (steps[i].given && steps[i].find) {
-            console.log(colors.green('given ' + steps[i].given));
-            let findKey = Object.keys(steps[i].find)[0]
-            let element = driver.findElement(By[findKey](steps[i].find[findKey]));
-            if (steps[i].sendKeys) {
-                element.sendKeys(steps[i].sendKeys);
+function setupTest(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i].setup == 'string') {
+            driver.get(array[i].setup);
+            continue;
+        }
+        throw 'no url to go to';
+    }
+}
+
+function performGiven(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].find) {
+            console.log(colors.green('given ' + array[i].given));
+            let findKey = Object.keys(array[i].find)[0]
+            let element = driver.findElement(By[findKey](array[i].find[findKey]));
+            if (array[i].sendKeys) {
+                element.sendKeys(array[i].sendKeys);
                 continue;
             }
-        } else if (steps[i].when && steps[i].find) {
+        }
+    }
+}
+
+function performWhen(steps) {
+    for (var i = 0; i < steps.length; i++) {
+        if (steps[i].find) {
             console.log(colors.green('when ' + steps[i].when));
             let findKey = Object.keys(steps[i].find)[0]
             let element = driver.findElement(By[findKey](steps[i].find[findKey]));
@@ -53,30 +63,33 @@ function runSteps(steps) {
             }
             if (steps[i].event) {
                 element[steps[i].event]();
+                continue;
             }
-        }
-        else if (steps[i].then) {
-            console.log(colors.green('when ' + steps[i].then));
-            if (steps[i].urlIs) {
-               let url = steps[i].urlIs;
-               driver.wait(until.urlIs(steps[i].urlIs, 10000))
-               driver.getCurrentUrl()
-               .then(currentUrl => {
-                    if (currentUrl === url) {
-                        console.log(colors.green('PASS'));
-                    }
-               });
-            }
-
+            throw 'need to make sure all when objects have a find method'
         }
     }
-    return result;
 }
 
-exports.runExperimental = function createStructuredArray(testArray) {
+function performThen(steps) {
+    for (let i = 0; i < steps.length; i++) {
+        console.log(colors.green('then ' + steps[i].then));
+        if (steps[i].urlIs) {
+            let url = steps[i].urlIs;
+            driver.wait(until.urlIs(steps[i].urlIs, 10000))
+            driver.getCurrentUrl()
+            .then(currentUrl => {
+                if (currentUrl === url) {
+                    console.log(colors.green('PASS'));
+                }
+            });
+        }
+    }
+}
+
+function formatTests(testArray) {
     var step = 0
     var finalArray = [[],[],[],[]];
-    for(var i = 0; i < testArray; i++) {
+    for(let i = 0; i < testArray.length; i++) {
         switch (step) {
             case 0:
                 if (testArray[i].setup) {
@@ -84,35 +97,39 @@ exports.runExperimental = function createStructuredArray(testArray) {
                     continue;
                 }
                 if (testArray[i].given) {
+                    finalArray[step + 1].push(testArray[i]);
                     step++;
                     continue;
                 }
                 throw 'need to order better';
             case 1:
+                console.log('step 1', testArray[i]);
                 if (testArray[i].given) {
                     finalArray[step].push(testArray[i]);
                     continue;
                 }
                 if (testArray[i].when) {
+                    finalArray[step + 1].push(testArray[i]);
                     step++;
                     continue;
                 }
                 throw 'need to order better';
             case 2:
-                if (testArray[i].when && finalArray[step].length === 0) {
-                    finalArray[step].push(testArray[i]);
-                    continue;
+                if (testArray[i].when) {
+                    throw 'cant have two whens in the same test';
                 }
                 if (testArray[i].then) {
+                    finalArray[step + 1].push(testArray[i]);
                     step++;
                     continue;
                 }
-                throw 'need to order better';
+                throw 'needs to be a when or then next'
             case 3:
-                if (testArray[i].then && finalArray[step].length === 0) {
-                    finalArray[step].push(testArray[i]);
-                    continue;
+                if (testArray[i].then) {
+                    throw 'cant have multile thens'
                 }
+                continue;
         }
     }
+    return finalArray;
 }
